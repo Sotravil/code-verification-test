b53d2c01d620a55d111861bef75e4168f52cf19a
Sotravil <78595995+Sotravil@users.noreply.github.com>
2023-04-21 00:33:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,91 @@
-const signupForm = document.querySelector('#signup-form');
-const confirmationForm = document.querySelector('#confirmation-form');
-const chatPage = document.querySelector('#chat-page');
-const userSpan = document.querySelector('#user');
+// Get the forms and error message elements
+const usernameForm = document.getElementById("username-form");
+const phoneForm = document.getElementById("phone-form");
+const confirmationForm = document.getElementById("confirmation-form");
+const backButton = document.getElementById("back-button");
+const chat = document.getElementById("chat");
+const usernameHeader = document.getElementById("username-header");
+const phoneHeader = document.getElementById("phone-header");
+const errorMessage = document.getElementById("error-message");
 
-signupForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const username = signupForm.username.value;
-	const phone = signupForm.phone.value;
-	sendConfirmationCode(username, phone);
-});
+// Set the initial state of the forms
+phoneForm.classList.add("hidden");
+confirmationForm.classList.add("hidden");
 
-confirmationForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const confirmationCode = confirmationForm['confirmation-code'].value;
-	verifyConfirmationCode(confirmationCode);
-});
-
-function sendConfirmationCode(username, phone) {
-	// Here, you would send a 6-digit code to the provided phone number using a service like Twilio
-	// For the purpose of this example, we'll generate a random code and display it in the console
-	const code = Math.floor(Math.random() * 900000) + 100000;
-	console.log(`Code sent to ${phone}: ${code}`);
-
-	// Display the chat page with the user's name and a message about the confirmation code
-	userSpan.textContent = username;
-	signupForm.reset();
-	chatPage.classList.remove('hidden');
+// Generate a 6-digit code and download the code file
+function generateCode() {
+  const code = Math.floor(100000 + Math.random() * 900000);
+  const file = new Blob([code.toString()], { type: "text/plain" });
+  const a = document.createElement("a");
+  const url = URL.createObjectURL(file);
+  a.href = url;
+  a.download = "code.txt";
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 0);
 }
 
-function verifyConfirmationCode(code) {
-	// Here, you would verify the user's provided code with the code that was sent to their phone
-	// For the purpose of this example, we'll assume the code is correct and display a message in the console
-	console.log('Confirmation code verified!');
-}
+// Handle form submissions
+usernameForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const username = event.target.elements.username.value;
+  if (username.trim() === "") {
+    errorMessage.textContent = "Please enter a username";
+    return;
+  }
+  usernameHeader.textContent = username;
+  usernameForm.classList.add("hidden");
+  phoneForm.classList.remove("hidden");
+});
+
+phoneForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const phoneNumber = event.target.elements.phone.value;
+  if (phoneNumber.trim() === "") {
+    errorMessage.textContent = "Please enter a phone number";
+    return;
+  }
+  phoneHeader.textContent = phoneNumber;
+  generateCode();
+  phoneForm.classList.add("hidden");
+  confirmationForm.classList.remove("hidden");
+});
+
+confirmationForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const code = event.target.elements.code.value;
+  if (code.trim() === "") {
+    errorMessage.textContent = "Please enter the confirmation code";
+    return;
+  }
+  // You can add your own logic to verify the code here
+  confirmationForm.classList.add("hidden");
+  chat.classList.remove("hidden");
+});
+
+// Handle back button click
+backButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  errorMessage.textContent = "";
+  confirmationForm.classList.add("hidden");
+  phoneForm.classList.remove("hidden");
+});
+
+// Handle message submission
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
+const messageList = document.getElementById("message-list");
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = messageInput.value.trim();
+  if (message !== "") {
+    const li = document.createElement("li");
+    li.textContent = message;
+    messageList.appendChild(li);
+    messageInput.value = "";
+  }
+});
